1824ff69600cd66245569257e6f9c147de055885
juunini <juuni.ni.i@gmail.com>
2021-10-01 23:55:04 +0900

    Initial Commit

---
+++ cypress/integration/app.test.ts
@@ -0,0 +1,10 @@
+describe('Home', () => {
+  it('should render index page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/');
+
+    cy.contains('h1', 'Welcome to Next.js!');
+  });
+});
+
+export {};
