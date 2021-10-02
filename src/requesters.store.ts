91c7d093fba04670071f48ecb6298bf71a743d5b
juunini <juuni.ni.i@gmail.com>
2021-10-03 02:18:15 +0900

    requesters strore에 remove를 추가하며 테스트 수정

--- src/requesters.store.ts
+++ src/requesters.store.ts
@@ -1,10 +1,13 @@
 import { atom } from 'jotai';
 
-const initialValue = atom<string[]>([]);
-
-const requesters = atom<string[], string>(
-  (get) => get(initialValue),
-  (get, set, name) => set(initialValue, [...get(initialValue), name]),
+export const requestersStore = atom<string[]>([]);
+export const addRequester = atom<string[], string>(
+  (get) => get(requestersStore),
+  (get, set, name) => set(requestersStore, get(requestersStore).concat(name)),
+);
+export const removeRequester = atom<string[], number>(
+  (get) => get(requestersStore),
+  (get, set, index) => set(requestersStore, get(requestersStore).filter((_, i) => i !== index)),
 );
 
-export default requesters;
+export default addRequester;
