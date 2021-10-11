dff707594b2be121150b152d3b068f7c38fc2b3c
juunini <juuni.ni.i@gmail.com>
2021-10-11 17:54:47 +0900

    E2E 테스트에 "섹스 신청서" 가 나오는지 확인합니다.

--- cypress/integration/app.test.ts
+++ cypress/integration/app.test.ts
@@ -2,5 +2,7 @@ describe('Home', () => {
   it('should render index page', () => {
     // Start from the index page
     cy.visit('http://localhost:3000/');
+
+    cy.contains('섹스 신청서');
   });
 });
