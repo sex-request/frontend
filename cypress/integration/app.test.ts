acfccd4f7ac982c3507062224643389ffc44de29
juunini <juuni.ni.i@gmail.com>
2021-10-11 17:56:15 +0900

    E2E 테스트의 설명을 한글로 변경합니다.

--- cypress/integration/app.test.ts
+++ cypress/integration/app.test.ts
@@ -1,5 +1,5 @@
-describe('Home', () => {
-  it('should render index page', () => {
+describe('랜딩페이지', () => {
+  it('제목이 나오는지 확인합니다.', () => {
     // Start from the index page
     cy.visit('http://localhost:3000/');
 
