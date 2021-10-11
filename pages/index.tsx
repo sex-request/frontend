f77541c1305e7c07c3f9abf10532c6d02690eb06
juunini <juuni.ni.i@gmail.com>
2021-10-11 15:40:45 +0900

    참여자 -> 참가자로 이름을 변경합니다.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import 제목 from 'src/Home/컴포넌트/제목';
+import 참가자 from 'src/Home/컴포넌트/참가자';
 
 export default function Home() {
   return (
     <>
       <제목 />
+      <참가자 />
     </>
   );
 }
--- src/Home/컴포넌트/참여자/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-
-import 참여자 from 'src/Home/컴포넌트/참여자';
-
-describe('참여자 컴포넌트', () => {
-  it('컴포넌트를 랜더링 합니다.', () => {
-    render(<참여자 />);
-  });
-});
