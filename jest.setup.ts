ab626b0d1b02ebd861e62f399efc2cbdf6973fcf
juunini <juuni.ni.i@gmail.com>
2021-10-14 22:06:44 +0900

    setup의 출처를 추가합니다.

--- jest.setup.ts
+++ jest.setup.ts
@@ -6,6 +6,7 @@
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 
+// https://github.com/vercel/next.js/issues/26749
 jest.mock('next/image', () => ({
   __esModule: true,
   default: () => 'Next image stub', // whatever
