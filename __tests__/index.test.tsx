deb325941781337d1178d10522de18ed74be3d62
Mo Kweon <kkweon@gmail.com>
2021-10-17 00:55:18 -0700

    feat: add query param supports
    so that I can just share via the URL
    
    **기타**
    
    - prettierrc.yml 추가
    - (.gitignore) /coverage 삭제 (중복됨)
    - (.gitignore) /.idea 추가

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Home from 'pages/index';
+import Home, { 값을_확인후_상태를_변경함_아님_말고 } from 'pages/index';
 
 test('랜딩페이지', () => {
   render(<Home />);
+});
+
+test('값을 확인후 상태를 변경함', () => {
+  const 값 = '값';
+  const 값_변경_함수 = jest.fn();
+
+  값을_확인후_상태를_변경함_아님_말고(값, 값_변경_함수);
+
+  expect(값_변경_함수).toHaveBeenCalledWith(값);
+});
+
+test('값을 확인후 상태를 변경할때, 사용 여부 함수도 변경함', () => {
+  const 값 = '값';
+  const 값_변경_함수 = jest.fn();
+  const 사용_여부_함수 = jest.fn();
+
+  값을_확인후_상태를_변경함_아님_말고(값, 값_변경_함수, 사용_여부_함수);
+
+  expect(값_변경_함수).toHaveBeenCalledWith(값);
+  expect(사용_여부_함수).toHaveBeenCalledWith(true);
+});
+
+test('값이 null 이면 상태를 변경 안함', () => {
+  const 값 = null;
+  const 값_변경_함수 = jest.fn();
+  값을_확인후_상태를_변경함_아님_말고(값, 값_변경_함수);
+
+  expect(값_변경_함수).not.toHaveBeenCalled();
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import 이름입력란 from 'src/랜딩페이지/컴포넌트/이름입력란';
 import 선택사항입력란 from 'src/랜딩페이지/컴포넌트/선택사항입력란';
@@ -7,6 +7,21 @@ import 선택사항입력란 from 'src/랜딩페이지/컴포넌트/선택사항
 import 스타일 from 'styles/랜딩페이지.module.css';
 
 import 로고 from 'public/logo.png';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export function 값을_확인후_상태를_변경함_아님_말고(
+  값: string | null,
+  값_변경_함수: (값: string) => void,
+  사용_여부_함수: ((사용_여부: boolean) => void) | null = null,
+) {
+  if (값) {
+    값_변경_함수(값);
+
+    if (사용_여부_함수) {
+      사용_여부_함수(true);
+    }
+  }
+}
 
 export default function 랜딩페이지(): JSX.Element {
   const [신청하는사람, 신청하는사람_수정] = useState<string>('');
@@ -19,6 +34,28 @@ export default function 랜딩페이지(): JSX.Element {
   const [장소_사용함, 장소_사용함_수정] = useState<boolean>(true);
   const [약속, 약속_수정] = useState<string>('');
   const [약속_사용함, 약속_사용함_수정] = useState<boolean>(true);
+
+  useEffect(() => {
+    const 파람 = new URLSearchParams(window.location.search);
+
+    const 신청하는사람_파람 = 파람.get('from');
+    값을_확인후_상태를_변경함_아님_말고(신청하는사람_파람, 신청하는사람_수정);
+
+    const 신청받는사람_파람 = 파람.get('to');
+    값을_확인후_상태를_변경함_아님_말고(신청받는사람_파람, 신청받는사람_수정);
+
+    const 시간_파람 = 파람.get('time');
+    값을_확인후_상태를_변경함_아님_말고(시간_파람, 시간_수정, 시간_사용함_수정);
+
+    const 날짜_파람 = 파람.get('date');
+    값을_확인후_상태를_변경함_아님_말고(날짜_파람, 날짜_수정, 날짜_사용함_수정);
+
+    const 장소_파람 = 파람.get('location');
+    값을_확인후_상태를_변경함_아님_말고(장소_파람, 장소_수정, 장소_사용함_수정);
+
+    const 약속_파람 = 파람.get('promise');
+    값을_확인후_상태를_변경함_아님_말고(약속_파람, 약속_수정, 약속_사용함_수정);
+  }, []);
 
   return (
     <div className={스타일.컨테이너}>
@@ -33,19 +70,9 @@ export default function 랜딩페이지(): JSX.Element {
 
       <header className={스타일.헤더}>
         <h1 className={스타일.제목}>
-          <Image
-            src={로고}
-            alt=""
-            width={60}
-            height={60}
-          />
+          <Image src={로고} alt="" width={60} height={60} />
           섹스 신청서
-          <Image
-            src={로고}
-            alt=""
-            width={60}
-            height={60}
-          />
+          <Image src={로고} alt="" width={60} height={60} />
         </h1>
       </header>
 
