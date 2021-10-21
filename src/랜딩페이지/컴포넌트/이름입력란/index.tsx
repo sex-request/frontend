bab2b681d528a5a4d45d1a2194cb6dfd2583773f
juunini <juuni.ni.i@gmail.com>
2021-10-22 02:29:19 +0900

    이름입력란을 리펙토링합니다.

--- src/랜딩페이지/컴포넌트/이름입력란/index.tsx
+++ src/랜딩페이지/컴포넌트/이름입력란/index.tsx
@@ -1,20 +1,30 @@
-import 스타일 from 'src/랜딩페이지/컴포넌트/이름입력란/이름입력란.module.sass';
+import { H2, Input } from 'src/랜딩페이지/컴포넌트/이름입력란/styles';
 
 interface Props {
-  value: string;
-  onChange: (value: string) => void;
+  신청하는사람: string;
+  신청하는사람_수정: (value: string) => void;
+  신청받는사람: string;
+  신청받는사람_수정: (value: string) => void;
 }
 
 export default function 이름입력란({
-  value,
-  onChange,
+  신청하는사람,
+  신청하는사람_수정,
+  신청받는사람,
+  신청받는사람_수정,
 }: Props): JSX.Element {
   return (
-    <input
-      className={스타일.입력란}
-      type="text"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-    />
+    <H2>
+      <Input
+        value={신청하는사람}
+        onChange={(e) => 신청하는사람_수정(e.target.value)}
+      />
+      가
+      <Input
+        value={신청받는사람}
+        onChange={(e) => 신청받는사람_수정(e.target.value)}
+      />
+      에게 섹스를 신청합니다.
+    </H2>
   );
 }
