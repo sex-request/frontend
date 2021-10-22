dcc8bce8eb9c125ea20e2c0b1c2cb193f7fd0f11
juunini <juuni.ni.i@gmail.com>
2021-10-22 20:13:50 +0900

    이미지를 클립보드로 가져가기 컴포넌트를 작성합니다.

---
+++ src/랜딩페이지/컴포넌트/이미지를_클립보드로_가져가기/index.tsx
@@ -0,0 +1,29 @@
+import type { ForwardedRef, RefObject } from 'react';
+
+import {
+  forwardRef,
+  useCallback,
+} from 'react';
+
+import { toBlob } from 'html-to-image';
+
+import 버튼 from 'src/공통/컴포넌트/버튼';
+
+const 이미지를_클립보드로_가져가기 = forwardRef((_, ref: ForwardedRef<HTMLElement>) => {
+  const onClick = useCallback(() => toBlob(
+    (ref as RefObject<HTMLElement>).current as HTMLElement,
+    { cacheBust: true },
+  )
+    .then((dataUrl) => navigator.clipboard?.write([
+      new ClipboardItem({
+        'image/png': dataUrl,
+      }),
+    ])),
+  [ref]);
+
+  return (
+    <버튼 onClick={onClick}>이미지를 클립보드로 가져가기</버튼>
+  );
+});
+
+export default 이미지를_클립보드로_가져가기;
