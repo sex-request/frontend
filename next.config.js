7c692f706f5c4295adbe473e4cc84e36577536b4
Juunini <juuni.ni.i@gmail.com>
2021-10-29 00:24:59 +0900

    URL과 Path를 구분해서 images.path 와 assetPrefix에 할당합니다.
    - #71
    - @mingkyme 님 께서 이슈로 기여해주셨습니다.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const isProduction = process.env.NODE_ENV === 'production';
-const productionURL = 'https://sex-request.github.io/frontend/';
+const productionURL = 'https://sex-request.github.io';
+const productionPath = '/frontend'
 const withMDX = require('@next/mdx')({
   extension: /\.mdx$/,
 });
@@ -9,9 +10,9 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   reactStrictMode: true,
   images: {
-    formats: ['image/avif', 'image/webp'],
     loader: 'imgix',
-    path: isProduction ? productionURL : 'http://localhost:3000/',
+    path: isProduction ? productionURL : 'http://localhost:3000',
   },
+  assetPrefix: isProduction ? productionPath : '',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 });
