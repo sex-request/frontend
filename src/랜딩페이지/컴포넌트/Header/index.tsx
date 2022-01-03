d491c8af2749dc33fa3823b18907d0a01cfbe9f9
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-01-03 23:17:27 +0900

    이미지파일 이름 오타를 수정합니다.

--- src/랜딩페이지/컴포넌트/Header/index.tsx
+++ src/랜딩페이지/컴포넌트/Header/index.tsx
@@ -6,9 +6,9 @@ export default function 헤더(): JSX.Element {
   return (
     <Header>
       <H1>
-        <Image src="/icon-192.png" alt="" width={60} height={60} />
+        <Image src="/icon-192x192.png" alt="" width={60} height={60} />
         섹스 신청서
-        <Image src="/icon-192.png" alt="" width={60} height={60} />
+        <Image src="/icon-192x192.png" alt="" width={60} height={60} />
       </H1>
     </Header>
   );
