887b284c3e0e8c35ee6e5f2288b97cae2d7b5705
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-01-03 23:10:14 +0900

    헤더 이미지를 public 에서 가져옵니다.

--- src/랜딩페이지/컴포넌트/Header/index.tsx
+++ src/랜딩페이지/컴포넌트/Header/index.tsx
@@ -1,15 +1,14 @@
 import Image from 'next/image';
 
 import { Header, H1 } from 'src/랜딩페이지/컴포넌트/Header/styles';
-import { logo } from 'src/assets/img';
 
 export default function 헤더(): JSX.Element {
   return (
     <Header>
       <H1>
-        <Image src={logo} alt="" width={60} height={60} />
+        <Image src="/icon-192.png" alt="" width={60} height={60} />
         섹스 신청서
-        <Image src={logo} alt="" width={60} height={60} />
+        <Image src="/icon-192.png" alt="" width={60} height={60} />
       </H1>
     </Header>
   );
